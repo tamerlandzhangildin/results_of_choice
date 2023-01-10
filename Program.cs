5eb02143dd76dc25b25ac1694247920414384993
Tamerlan <dzhangtim@list.ru>
2023-01-10 14:39:24 +0300

    new programm

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
+string[] array2 = new string[array1.Length];
+
+void ArrayWith2IF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayWith2IF(array1, array2);
+PrintArray(array2);
